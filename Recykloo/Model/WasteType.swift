3ad073f4194917f82f96b85d4892ad1d90b92a8d
naufalmufidf <117738536+naufalmufidf@users.noreply.github.com>
2024-07-18 12:11:30 +0700

    commit view waste type sama waste detail

---
+++ Recykloo/Model/WasteType.swift
@@ -0,0 +1,17 @@
+//
+//  WasteType.swift
+//  Recykloo
+//
+//  Created by palpedpad on 18/07/24.
+//
+
+import Foundation
+import UIKit
+
+struct WasteType {
+    var id: UUID
+    var name: String
+    var image: String
+    var point: Int
+    var category: String
+}
